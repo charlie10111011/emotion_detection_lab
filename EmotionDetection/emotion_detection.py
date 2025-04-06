5d4c2aeccbd672eecd4eba1ea9a226259ebfdabf
charlie10111011 <clee.ying@gmail.com>
2025-04-06 00:04:56 -0400

    updated function script

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,27 +2,28 @@ import json
 import requests
 
 def emotion_detector(text_to_analyze):  # Define the emotion detection function that takes the string input
-   # Emotion detection service URL
+    # Emotion detection service URL
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-   # Create a dictionary of the input string
+    # Create a dictionary of the input string
     myobj = { "raw_document": { "text": text_to_analyze } }
-   # Set the headers required for the API request
+    # Set the headers required for the API request
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-   # Send a POST request to the API with the string and header
+    # Send a POST request to the API with the string and header
     response = requests.post(url, json = myobj, headers=header) 
    # Assign the status code to a variable
     status_code = response.status_code
-
-   # Parse the JSON response 
+   
+    # Parse the JSON response 
     formatted_response = json.loads(response.text)
 
-   # Create a dictionary for the response
+    # Create a dictionary for the response
     emotion_dict = {}
    
-   # Generate the formatted response
+    # Process the json formatted response
     if status_code == 200:
         emotion_dict = formatted_response['emotionPredictions'][0]['emotion']
-        dominant_emotion_score = max(emotion_dict['anger'], emotion_dict['disgust'], emotion_dict['fear'], emotion_dict['joy'], emotion_dict['sadness'])
+        dominant_emotion_score = max(emotion_dict['anger'], emotion_dict['disgust'], emotion_dict['fear'],
+                                emotion_dict['joy'], emotion_dict['sadness'])
         dominant_emotion = ''
         if dominant_emotion_score == emotion_dict['anger']:
             dominant_emotion = 'anger'
@@ -41,5 +42,5 @@ def emotion_detector(text_to_analyze):  # Define the emotion detection function 
         emotion_dict['joy'] = None
         emotion_dict['sadness'] = None
         emotion_dict['dominant_emotion'] = None
-    
+  
     return emotion_dict
